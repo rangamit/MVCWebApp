837f109f27b0d0ad7a24350f368afd19d548319e
rangamit <98092168+rangamit@users.noreply.github.com>
2022-01-20 17:17:26 +0530

    CRUD operations using Static list object

---
+++ MVCWebApp/Controllers/OrganisationController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCWebApp.Models;
+using MVCWebApp.ViewModels.Organisation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCWebApp.Controllers
+{
+    public class OrganisationController : Controller
+    {
+        public static List<Organisation> staticOrganizations = new List<Organisation>();
+        public IActionResult Index()
+        {
+            IndexViewModel organisationViewModel = new IndexViewModel();
+
+            organisationViewModel.Organisations = staticOrganizations;
+
+            return View(organisationViewModel);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add([Bind] CreateViewModel createViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                staticOrganizations.Add(new Organisation
+                {
+                    Id = Guid.NewGuid(),
+                    City = createViewModel.City,
+                    Country = createViewModel.Country,
+                    Name = createViewModel.Name,
+                    State = createViewModel.State,
+                });
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Update(Guid id)
+        {
+            var org = staticOrganizations.FirstOrDefault(o => o.Id == id);
+
+            return View(new UpdateViewModel
+            {
+                Id = org.Id,
+                City = org.City,
+                Country = org.Country,
+                Name = org.Name,
+                State = org.State,
+            });
+        }
+
+        [HttpPost]
+        public IActionResult Update([Bind] UpdateViewModel updateViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var org = staticOrganizations.FirstOrDefault(o => o.Id == updateViewModel.Id);
+                if (org != null)
+                {
+                    org.City = updateViewModel.City;
+                    org.Country = updateViewModel.Country;
+                    org.Name = updateViewModel.Name;
+                    org.State = updateViewModel.State;
+                }
+
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(Guid id)
+        {
+            var org = staticOrganizations.FirstOrDefault(o => o.Id == id);
+
+            return View(new DeleteViewModel
+            {
+                Id = org.Id,
+                Name = org.Name
+            });
+        }
+
+        [HttpPost]
+        public IActionResult Delete([Bind] DeleteViewModel deleteViewModel)
+        {
+            var org = staticOrganizations.FirstOrDefault(o => o.Id == deleteViewModel.Id);
+            if (org != null)
+            {
+                staticOrganizations.Remove(org);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVCWebApp/ViewModels/Organisation/IndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MVCWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCWebApp.ViewModels.Organisation
+{
+    public class IndexViewModel
+    {
+        public List<Models.Organisation> Organisations { get; set; }
+    }
+}
