324840d7b4ffd386fe18fd0c21c76616a0db5256
rangamit <98092168+rangamit@users.noreply.github.com>
2022-01-29 13:26:15 +0530

    Added webapi project

---
+++ WebAPI/Controllers/OrganisationController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Data;
+using WebAPI.Models;
+using WebAPI.ViewModels.Organisation;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrganisationController : ControllerBase
+    {
+        private readonly ApplicationDBContext _dbContext;
+
+        public OrganisationController(ApplicationDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<Organisation> Get()
+        {
+            var organsiations = _dbContext.Organisations.ToList();
+            return organsiations;
+        }
+
+        [HttpPost("AddOrgnisation")]
+        public IActionResult AddOrgnisation([Bind] CreateViewModel createViewModel)
+        {
+            var org = new Organisation
+            {
+                City = createViewModel.City,
+                Country = createViewModel.Country,
+                Name = createViewModel.Name,
+                State = createViewModel.State,
+            };
+            _dbContext.Organisations.Add(org);
+            _dbContext.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ WebAPI/Data/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace WebAPI.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Organisation> Organisations { get; set; }
+
+    }
+}
